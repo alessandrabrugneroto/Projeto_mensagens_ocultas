5ca7061e1f703f10733ff37fd0ceed9debbc20cc
alessandra <brugneroto.alessandra@.com.br>
2021-04-27 19:27:58 -0300

    projeto

--- projeto.js
+++ projeto.js
@@ -1,99 +1,63 @@
-window.onload = () => {
-    document.getElementById('cypher').disabled = true;
-    document.getElementById('decypher').disabled = true;
+function cifrar() {
+    let texto = document.getElementById("texto").value;
+    let numDesloc = parseInt(document.getElementById("deslocamento").value);
 
-    document.getElementById('message-box').addEventListener('input', () => {
-        enableButton();
-        addTextAreaRows();
-        clearResults();
-    });
-    document.getElementById('cypher').addEventListener('click', cypher);
-    document.getElementById('decypher').addEventListener('click', decypher);
-};
+    document.getElementById("textocifrado").innerHTML = cypherCifrar(texto, numDesloc);
+}
 
-const enableButton = () => {
-    if (getMessage().trim().length !== 0) {
-        document.getElementById('cypher').disabled = false;
-        document.getElementById('decypher').disabled = false;
-    } else {
-        document.getElementById('cypher').disabled = true;
-        document.getElementById('decypher').disabled = true;
+function cypherCifrar(texto, numDesloc) {
+    let textoArray = [];
+    let deslocamentoArray = [];
+    let textoCodificado = "";
+
+    for (let i = 0; i < texto.length; i++) {
+        textoArray.push(texto.charCodeAt(i));
     }
-};
 
-const addTextAreaRows = () => {
-    const txtArea = document.getElementById('message-box');
-    txtArea.style.height = '8vh';
-    txtArea.style.height = txtArea.scrollHeight + 'px';
-};
-
-const clearResults = () => {
-    if (document.getElementById('p-group'))
-        document.getElementById('results').removeChild(document.getElementById('p-group'));
-};
-
-const cypher = () => {
-    const cypheredCharCodes = cypherCharcodes(getCharcodesFromMessage());
-    const resultMessage = transformIntoLetters(cypheredCharCodes);
-    const method = 'Codificada';
-    showResultOnPage(resultMessage, method);
-    clearTextArea();
-};
-
-const decypher = () => {
-    const decypheredCharCodes = decypherCharcodes(getCharcodesFromMessage());
-    const resultMessage = transformIntoLetters(decypheredCharCodes);
-    const method = 'Descodificada';
-    showResultOnPage(resultMessage, method);
-    clearTextArea();
-};
-
-const getMessage = () => document.getElementById('message-box').value;
-const getCharcodesFromMessage = () => getMessage().split('').map(val => val.charCodeAt());
-
-const cypherCharcodes = charcodesArray => charcodesArray.map((charCode, i) => {
-    if (charCode > 47 && charCode < 58) {
-        charCode = (charCode - 48 + 33) % 10 + 48;
-    } else if (charCode > 64 && charCode < 91) {
-        charCode = (charCode - 65 + 33) % 26 + 65;
+    for (j = 0; j < textoArray.length; j++) {
+        if (textoArray[j] >= 65 && textoArray[j] <= 90) {
+            deslocamentoArray.push((((textoArray[j] - 65 + numDesloc) % 26) + 65));
+        } else if (textoArray[j] >= 97 && textoArray[j] <= 122) {
+            deslocamentoArray.push((((textoArray[j] - 97 + numDesloc) % 26) + 97));
+        } else {
+            deslocamentoArray.push(textoArray[j]);
+        }
     }
-    if (charCode > 96 && charCode < 192) {
-        charCode = (charCode - 97 + 33) % 26 + 97;
-    } else if (charCode > 191 && charCode < 222) {
-        charCode = (charCode - 192 + 33) % 30 + 192;
-    } else if (charCode > 221 && charCode <= 255) {
-        charCode = (charCode - 222 + 33) % 34 + 222;
+
+    for (g = 0; g < deslocamentoArray.length; g++) {
+        textoCodificado += String.fromCharCode(deslocamentoArray[g]);
     }
-    return charCode;
-});
+    return textoCodificado;
+}
 
-const decypherCharcodes = charcodesArray => charcodesArray.map((charCode, i) => {
-    if (charCode > 47 && charCode < 58) {
-        charCode = (charCode - 57 - 33) % 10 + 57;
-    } else if (charCode > 64 && charCode < 91) {
-        charCode = (charCode - 90 - 33) % 26 + 90;
-    } else if (charCode > 96 && charCode < 192) {
-        charCode = (charCode - 122 - 33) % 26 + 122;
-    } else if (charCode > 191 && charCode < 222) {
-        charCode = (charCode - 221 - 33) % 30 + 221;
-    } else if (charCode > 221 && charCode <= 255) {
-        charCode = (charCode - 255 - 33) % 34 + 255;
+function decifrar() {
+    let texto = document.getElementById("texto").value;
+    let numDesloc = parseInt(document.getElementById("deslocamento").value);
+
+    document.getElementById("textocifrado").innerHTML = cypherDec(texto, numDesloc);
+}
+
+function cypherDec(texto, numDesloc) {
+    let textoArray = [];
+    let deslocamentoArray = [];
+    let textoCodificado = "";
+
+    for (let i = 0; i < texto.length; i++) {
+        textoArray.push(texto.charCodeAt(i));
     }
-    return charCode;
-});
 
-const transformIntoLetters = charcodesArray => String.fromCharCode.apply(this, charcodesArray);
+    for (j = 0; j < textoArray.length; j++) {
+        if (textoArray[j] >= 65 && textoArray[j] <= 90) {
+            deslocamentoArray.push((((textoArray[j] - 90 - numDesloc) % 26) + 90));
+        } else if (textoArray[j] >= 97 && textoArray[j] <= 122) {
+            deslocamentoArray.push((((textoArray[j] - 122 - numDesloc) % 26) + 122));
+        } else {
+            deslocamentoArray.push(textoArray[j]);
+        }
+    }
 
-const showResultOnPage = (resultMessage, method) => {
-    return document.getElementById('results').innerHTML = `
-  <div id="p-group">
-    <p class="result-fixed-message w-100">Esta é a sua mensagem ${method}:</p>
-    <p class="result-text w-100">${resultMessage}</p>
-  </div>`;
-};
-
-const clearTextArea = () => {
-    document.getElementById('message-box').value = '';
-    enableButton();
-    addTextAreaRows();
-};
+    for (g = 0; g < deslocamentoArray.length; g++) {
+        textoCodificado += String.fromCharCode(deslocamentoArray[g]);
+    }
+    return textoCodificado;
+}
